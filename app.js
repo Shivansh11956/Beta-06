a9973060ec7ba22e113568459b79b545224ce6ee
Shivansh11956 <157223986+Shivansh11956@users.noreply.github.com>
2025-11-01 02:08:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+let bcrypt = require('bcrypt')
+let jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
+
+
+
+app.set('view engine','ejs');
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.use(cookieParser())
+require('dotenv').config();
+
+
+app.get('/',(req,res) => {
+    res.render("organiser")
+})
+
+app.listen(3000,()=>{
+    console.log('running');
+});
